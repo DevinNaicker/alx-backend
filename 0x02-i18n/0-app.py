aae85344ae6c0be995338a292efcb29e0458824e
Devin Naicker <thirushandevinnaicker@gmail.com>
2025-05-20 22:17:08 +0200

    0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
-"""A simple flask app
-"""
-
-
+"""Basic Flask app"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    """_summary_
-    """
-    return render_template('0-index.html')
+def index():
+    """Root route"""
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run(debug=True)
